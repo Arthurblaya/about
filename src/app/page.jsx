0cc6ca3b6a986cd4d098fbde4cae879b6063ae2b
ArthurBlaya <arturblaya@gmail.com>
2023-12-03 21:29:20 +0100

    Contact me

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,23 @@ import Image from "next/image";
 
 
 export default function Home() {
+
+  const mailTo = "arturblaya@gmail.com"
+  const subject = "Connecting from https://arturblaya.bio"
+  const body = `
+  
+  Hi Artur,
+
+  I hope you're doing well. While checking out your profile at https://arturblaya.bio, [mention something specific] caught my eye.
+
+  I'd love to connect with you regarding [reason for your interest]. Could we schedule a brief call or meeting?  
+
+  Thanks,
+
+  [Name]
+  [Contact Info]
+  `
+
   return (
     <main className="mw--xl c">
       <section className="c--c">
@@ -27,7 +44,12 @@ export default function Home() {
         </p>
       </section>
 
-      {/* <ContactMe/> */}
+      <button>
+
+        <a href={`mailto:${mailTo}?subject=${encodeURI(subject)}&body=${encodeURI(body)}`}>Contact me</a>
+      </button>
+
     </main>
   )
 }
+
